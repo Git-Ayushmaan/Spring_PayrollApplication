c56308b9b4c561d19f55791539674cc97fccdee7
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-02-27 11:41:13 +0530

    [Ayushmaan Tiwari]Add.Using Annotation @Slf4j at Class Level

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
@@ -2,40 +2,47 @@ package com.bridgelabz.employeepayrollapp.controller;
 
 import com.bridgelabz.employeepayrollapp.model.Employee;
 import com.bridgelabz.employeepayrollapp.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/employeepayrollservice")
-class EmployeelController {
+public class EmployeeController {
 
     @Autowired
     private EmployeeService service;
 
     @GetMapping("/")
     public List<Employee> getAllEmployees() {
+        log.info("Received request to fetch all employees");
         return service.getAllEmployees();
     }
 
     @GetMapping("/get/{id}")
     public Employee getEmployeeById(@PathVariable Long id) {
+        log.debug("Fetching employee with ID: {}", id);
         return service.getEmployeeById(id);
     }
 
     @PostMapping("/create")
     public Employee createEmployee(@RequestBody Employee employee) {
+        log.info("Creating employee: {}", employee);
         return service.createEmployee(employee);
     }
 
     @PutMapping("/update/{id}")
     public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        log.info("Updating employee with ID: {}", id);
         return service.updateEmployee(id, employee);
     }
 
     @DeleteMapping("/delete/{id}")
     public void deleteEmployee(@PathVariable Long id) {
+        log.warn("Deleting employee with ID: {}", id);
         service.deleteEmployee(id);
     }
 }
